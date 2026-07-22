2bf2bc226ab8366a95f356a49ccbb04e2aa5ecf8
suraimu87 <ktc2241010004@edu.kyoto-tech.ac.jp>
2026-07-22 18:12:19 +0900

    ChangeParm

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,7 +21,7 @@ public class Player : MonoBehaviour
     private Vector3 rotateTarget;
     private bool isGrounded;
 
-
+    private Vector2 accelVec;
 
     private void Awake()
     {
@@ -52,68 +52,22 @@ public class Player : MonoBehaviour
         {
 
 
-            var accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
+            accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
 
-            var cameraDir = playerInput.camera.transform.forward;
-            cameraDir.y = 0;
-            cameraDir = cameraDir.normalized;
-
-            var cameraRight = playerInput.camera.transform.right;
-
-            var accelVec3D =
-                cameraDir * accelVec.y * accel
-                + cameraRight * accelVec.x * accel;
-            rb.AddForce(accelVec3D, ForceMode.Acceleration);
-
-            // プレイヤーの向きを変える
-            if (accelVec3D != Vector3.zero)
-            {
-                rotateTarget = accelVec3D.normalized;
-            }
-            // 前方向をコピーしておく
-            Vector3 forward = transform.forward;
-
-
-
-            // 上方向を固定
-            transform.up = Vector3.up;
-
-
-
-            // 前方向をターゲットに向かって補間
-
-            var tempForward = Vector3.Slerp(forward, rotateTarget, rotateSpeed * Time.deltaTime);
-
-            if (tempForward != Vector3.zero)
-            {
-                transform.forward = tempForward;
-            }
-
-
-            // アニメーターのMoveSpeedパラメータに Rigidbody の移動速度の大きさを与える
-            Vector3 velocityXZ = rb.linearVelocity;
-            velocityXZ.y = 0;
-            animator.SetFloat("MoveSpeed", velocityXZ.magnitude);
-
-            // ジャンプ
-            if (playerInput.actions["Jump"].WasPressedThisFrame() && isGrounded)
-            {
-                Vector3 jumpVec = new Vector3(0, jumpSpeed, 0);
-                rb.AddForce(jumpVec, ForceMode.VelocityChange);
-            }
+            Jump();
+            
         }
     }
 
     private void FixedUpdate()
     {
         // 減衰を地上と空中で変える
+        ApplyDamping();
+
         if (isGrounded)
         {
-            rb.linearDamping = groundDamping;
-        }
-        else
-        {
-            rb.linearDamping = airDamping;
+            Move();
+            UpdateAnimationParm();
         }
 
         // 物理計算中に接地判定を行うため、一旦ここで false にしておく
@@ -131,4 +85,78 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Move()
+    {
+        var cameraDir = GetCameraXZVector();
+
+        var cameraRight = playerInput.camera.transform.right;
+
+        var accelVec3D =
+            (accel * accelVec.y * cameraDir)
+            + (accel * accelVec.x * cameraRight);
+        rb.AddForce(accelVec3D, ForceMode.Acceleration);
+
+        // プレイヤーの向きを変える
+        if (accelVec3D != Vector3.zero)
+        {
+            rotateTarget = accelVec3D.normalized;
+        }
+        // 前方向をコピーしておく
+        Vector3 forward = transform.forward;
+
+
+
+        // 上方向を固定
+        transform.up = Vector3.up;
+
+
+
+        // 前方向をターゲットに向かって補間
+
+        var tempForward = Vector3.Slerp(forward, rotateTarget, rotateSpeed * Time.deltaTime);
+
+        if (tempForward != Vector3.zero)
+        {
+            transform.forward = tempForward;
+        }
+    }
+
+    private void Jump()
+    {
+        // ジャンプ
+        if (playerInput.actions["Jump"].WasPressedThisFrame() && isGrounded)
+        {
+            Vector3 jumpVec = new Vector3(0, jumpSpeed, 0);
+            rb.AddForce(jumpVec, ForceMode.VelocityChange);
+        }
+    }
+
+    private Vector3 GetCameraXZVector()
+    {
+        var cameraDir = playerInput.camera.transform.forward;
+        cameraDir.y = 0;
+        cameraDir = cameraDir.normalized;
+
+        return cameraDir;
+    }
+
+    private void ApplyDamping()
+    {
+        if (isGrounded)
+        {
+            rb.linearDamping = groundDamping;
+        }
+        else
+        {
+            rb.linearDamping = airDamping;
+        }
+    }
+
+    private void UpdateAnimationParm()
+    {
+        // アニメーターのMoveSpeedパラメータに Rigidbody の移動速度の大きさを与える
+        Vector3 velocityXZ = rb.linearVelocity;
+        velocityXZ.y = 0;
+        animator.SetFloat("MoveSpeed", velocityXZ.magnitude);
+    }
 }
